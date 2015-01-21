54b41b17d3b4670197e7cd6c847460f4f62738bc
Matthew W <matthew.woolf@mailonline.co.uk>
2015-01-21 15:58:05 +0000

    Fix Reporter scope

--- reporter.js
+++ reporter.js
@@ -9,39 +9,41 @@ module.exports = function Reporter(config){
 	config.errorTTL = config.errorTTL || 600000 ;
 	config.errorThreshold = 400 ;
 
-	Reporter.monitor = function(req,res,next){
-		var writer = res.write ;
-		var responseBody = [] ;
-		res.write = function(chunk) {
-			if (res.statusCode>=errorThreshold)
-				responseBody.push(chunk) ;
-			writer.apply(this,arguments) ;
-		}
-		res.on('finish',function(){
-			if (res.statusCode>=errorThreshold)
-				last = {
-					url:req.originalUrl,
-					status:res.statusCode,
+	return {
+		monitor:function(req,res,next){
+			var writer = res.write ;
+			var responseBody = [] ;
+			res.write = function(chunk) {
+				if (res.statusCode>=errorThreshold)
+					responseBody.push(chunk) ;
+				writer.apply(this,arguments) ;
+			}
+			res.on('finish',function(){
+				if (res.statusCode>=errorThreshold)
+					last = {
+						url:req.originalUrl,
+						status:res.statusCode,
+						timestamp:Date.now(), 
+						response:responseBody.map(function(x){ 
+							return x.toString()}).join("")} ;
+			}) ;
+			next && next() ;
+		},
+
+		lastError:function(req,res,next){
+			if (last) last.age = Date.now()-last.timestamp ;
+			if (last && last.age<config.errorTTL) 
+				res.status(400).json({status:"ERROR",lastError:last}) ;
+			else
+				res.json({status:"OK", lastError:last}) ;
+		},
+
+		setLastError:function(info){
+			last = {
+					status:999,
 					timestamp:Date.now(), 
-					response:responseBody.map(function(x){ 
-						return x.toString()}).join("")} ;
-		}) ;
-		next && next() ;
-	};
-
-	Reporter.lastError = function(req,res,next){
-		if (last) last.age = Date.now()-last.timestamp ;
-		if (last && last.age<config.errorTTL) 
-			res.status(400).json({status:"ERROR",lastError:last}) ;
-		else
-			res.json({status:"OK", lastError:last}) ;
-	};
-
-	Reporter.setLastError = function(info){
-		last = {
-			status:999,
-			timestamp:Date.now(), 
-			error:info
-		} ;
-	};
+					error:info
+			} ;
+		};
+	}
 }
